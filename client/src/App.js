01434e2b54f1140c9d7412e78932ccd3284fc443
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-03-27 13:08:19 +0200

    adding routes, route and browserrouting

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 export default function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
-    <div>App</div>
+    <div>
+      <p>{!data ? "Loading..." : data}</p>
+      <Routes>
+        <Route path="/home" element={HomePage} />
+        <Route path="/login" element={LoginPage} />
+        <Route path="/register" element={RegisterPage} />
+      </Routes>
+    </div>
   )
 }
