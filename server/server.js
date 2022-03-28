78579eaeb59dea82c1a4559bfc6356edc6d141bb
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-03-28 17:29:43 +0200

    moving folders

--- server/server.js
+++ server/server.js
@@ -3,19 +3,14 @@ const app = express()
 const port = 3001 
 const cors = require("cors")
 const mongoose = require("mongoose")
-const cookieParser = require("cookie-parser")
-const bodyParser = require("body-parser")
 
-const secret = 'secret'
 
-app.use(cookieParser())
-app.use(bodyParser.json({extended:true}))
 app.use(cors())
 app.use(express.json())
 
 app.get("/api", (req, res) => {
-    res.send("hi from server")
-})
+  res.json({ message: "Hello from server!" });
+});
 
 
 mongoose
@@ -26,8 +21,6 @@ mongoose
   .then(console.log("mongoDB connected"))
   .catch((err) => console.log(err));
 
-const db = mongoose.connection
-db.on("error", console.error.bind(console, "connection error:"))
 app.listen(port, () => {
     console.log(`Express started on port ${port}`)
 })
