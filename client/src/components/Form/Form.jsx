c99d3e58e68ef1dddeca84b7c40994aafee21253
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-04-01 17:00:29 +0200

    form for posts

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Form = () => {
+    const [todoData, setTodoData] = useState({
+        user: "", entry: "", tags: "" 
+    })
 
-    const handleSubmit = () => {
+    const handleOnSubmit = () => {
 
     }
 
+    const clear = () => {
+        
+    }
+
+
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="entry"/>
-            
+        <form onSubmit={handleOnSubmit}>
+            <textarea 
+                name="user" 
+                rows="10"
+                cols="10"    
+                value={todoData.user}
+                onChange={(e) => setTodoData({ ...todoData, user: e.target.value })}
+            />
+            <textarea 
+                name="entry" 
+                rows="10"
+                cols="10"    
+                value={todoData.entry}
+                onChange={(e) => setTodoData({ ...todoData, entry: e.target.value })}
+            />
+            <textarea 
+                name="tags" 
+                rows="10"
+                cols="10"    
+                value={todoData.tags}
+                onChange={(e) => setTodoData({ ...todoData, tags: e.target.value })}
+            />
+            <button type="submit">Submit</button>
+            <button type={clear}>Clear</button>
         </form>
     )
 }
