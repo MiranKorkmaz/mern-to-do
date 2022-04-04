93dd7ded67a454f5c286c31c4ebceea4116e42eb
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-04-04 22:35:44 +0200

    Sign up/Sign in form created

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
 export default function Auth() {
-  const state = null
+  const isSignup = true
+
+  const handleOnSubmit = () => {
+
+  }
+
+  const handleOnChange = () => {
+
+  }
+
   return (
     <div>
-        AUTH 
+      {isSignup ? "Sign Up" : "Sign In"}
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          name="username"
+          label="username"
+          placeholder="Username"
+          handleOnChange={handleOnChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          label="password"
+          placeholder="Password"
+          handleOnChange={handleOnChange}
+          required
+        />
+        <button type="submit">{isSignup ? "Sign Up" : "Sign In"}</button>
+      </form>
     </div>
   )
 }
